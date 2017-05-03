beaf0c57c9fcda0dc55535642470764a80afaa64
Nate Tangsurat <e4r7hbug@gmail.com>
2017-05-03 13:33:02 -0500

    feat: Verbose mode for logging

--- src/otter/__main__.py
+++ src/otter/__main__.py
@@ -18,9 +18,13 @@ def main():
 
 
 @click.group()
-def root():
+@click.option('-v', '--verbose', count=True, help='More verbose, use up to three times')
+def root(verbose):
     """Ready for takeoff."""
     click.secho('Aye aye, otter away!', fg='green')
+
+    verbosity = logging.root.getEffectiveLevel() - 10 * verbose or 1
+    logging.getLogger(__package__).setLevel(verbosity)
 
 
 def link_commands(pilot_root_group):
